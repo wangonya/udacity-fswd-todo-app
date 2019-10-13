f282ecb2990106d946e8602a57adae77a42fe78a
= <kwangonya@gmail.com>
2019-10-13 11:04:11 +0300

    fetch todos from db

--- app.py
+++ app.py
@@ -4,8 +4,25 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+    done = db.Column(db.Boolean, default=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+
+db.create_all()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', data='todos here')
+    return render_template('index.html',
+                           todos=Todo.query.all())
