48594dc710a954910736c7a0f727311bf4087544
= <kwangonya@gmail.com>
2019-10-13 09:24:59 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', data='todos here')
