5ba1cdb355299d1af2e1ef9d088a69fcc7970505
wangonya <kwangonya@gmail.com>
2019-10-27 06:06:43 +0300

    show lists in template

--- app.py
+++ app.py
@@ -38,9 +38,9 @@ def index():
 @app.route('/lists/<list_id>')
 def get_list_todos(list_id):
     return render_template('index.html',
-                          todos=Todo.query.filter_by(
-                              list_id=list_id).order_by(
-                              'id').all())
+                           lists=TodoList.query.all(),
+                           todos=Todo.query.filter_by(
+                               list_id=list_id).order_by('id').all())
 
 
 @app.route('/todos/create', methods=['POST'])
